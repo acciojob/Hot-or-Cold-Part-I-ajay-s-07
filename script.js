1c427dbde491c200f23c3f3b9b36bfb4afeb4e78
Ajay Sarkate <115990815+ajay-s-07@users.noreply.github.com>
2023-02-11 11:08:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //your code here
+document.getElementById("btn").addEventListener("click", function() {
+  var randomNumber = Math.floor(Math.random() * 41) - 20;
+  document.getElementById("num").innerHTML = randomNumber;
+});
 

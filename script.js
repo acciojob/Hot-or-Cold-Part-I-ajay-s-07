e0cf9ecd06374937e997d4993f9c223ed6f1deb2
Ajay Sarkate <115990815+ajay-s-07@users.noreply.github.com>
2023-02-12 15:51:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-//your code here
-var targetNumber;
-
-document.getElementById("btn").addEventListener("click", function() {
-  targetNumber = Math.floor(Math.random() * 41) - 20;
-  document.getElementById("guess").innerHTML = "";
-  document.getElementById("respond").innerHTML = "";
-});
-
-document.getElementById("guess").addEventListener("input", function() {
-  var guess = parseInt(document.getElementById("guess").innerHTML);
-  if (Math.abs(targetNumber - guess) <= 5) {
-    document.getElementById("respond").innerHTML = "Hot";
-  } else {
-    document.getElementById("respond").innerHTML = "Cold";
-  }
-});
+var randomNum;
+var input;
+function randomNumGenerator(){
+	randomNum = Math.floor(Math.random()*41) - 20;
+	document.getElementById("num").innerHTML = randomNum;
+	guessName();
+	print();
+}
+function guessName() {
+	input = parseInt(document.getElementById("guess").value);	
+}
+function print(){
+	if(Math.abs(randomNum - input) <= 5){
+		document.getElementById("respond").innerHTML = "Hot";
+	}
+	else{
+		document.getElementById("respond").innerHTML = "Cold";
+	}
+}
 
